2b74e0412caf5f300d1d1559b980f54668a0239d
ytao1015 <42874947+ytao1015@users.noreply.github.com>
2020-02-08 15:50:19 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,48 @@
-/*Created by Yi Tao on 1/19/20.
- *This program is to receives five double
- *type inputs from user and
- *to calculate the sum, average,product
- *and the absolute value of each number to the console
-*/
-
+//  Created by Yi Tao on 1/26/20.
+//  Copyright © 2020 Yi Tao. All rights reserved.
+//
+/* This program is to analyse the strength of the password:A hard password
+ * has at least 4 digits, at least 3 punctuation characters, and at least
+ * 8 letters of the alphabet of which at least 2 are uppercase.
+ * A medium password has at least 2 digits, at least 1 punctuation character,
+ * and at least 5 letters of the alphabet of which at least 1 is uppercase.
+ * All other passwords are weak.*/
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
-int main()
-{
-    double num1, num2, num3,num4,num5,sum;
-    cout<< "Enter five numbers: ";
-    cin >> num1 >> num2 >> num3 >> num4 >> num5;
-    sum = num1+num2+num3+num4+num5;
-    cout<<"Sum: "<< sum << endl;
-    cout<< "Average: "<< sum/5 << endl;
-    cout<< "Product: "<< num1 * num2*num3*num4*num5 << endl;
-    cout<< "Absolute Values: "<< abs(num1)<< " "<<abs(num2) << " "<< abs(num3) <<" "<< abs(num4) << " "<< abs(num5) << endl;
-    cout << "Done"<< endl;
+
+int main() {
+    string password;
+    int count_punct = 0,count_digits = 0,count_alpha = 0,count_uppercase = 0;
+    cout << "Password Analyzer"<< endl << endl
+    << "Enter password:  ";
+    cin  >> password;
+    cout << endl;
+    
+    for(char c : password)
+    {
+        if(isdigit(c))
+            count_digits++;
+        else if(isalpha(c)){
+            count_alpha++;
+            if(isupper(c))
+                count_uppercase++;
+        }
+        else
+            count_punct++;
+    }
+    
+    if(count_digits >=4 && count_punct >=3 && count_alpha >=8 && count_uppercase >=2)
+        cout << "Strength: Hard" << endl << endl;
+    else if(count_digits >=2 && count_punct >=1 && count_alpha >=5 && count_uppercase >=1)
+        cout<< "Strength: Medium" << endl <<endl;
+    else
+        cout<<"Strength: Weak" << endl << endl;
+    
+    cout << "Bye!" << endl << endl ;
+    
+    
+    
     return 0;
 }
-
